bd2c5cca8864669d409215acfcd0fc1ad051957e
imatry <ousssamafoot@gmail.com>
2025-03-11 11:23:04 +0000

    Update and rename Euler 07.c to Euler 07:10 001st Prime.c

--- Euler 07:10 001st Prime.c
+++ Euler 07:10 001st Prime.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 
-// Fonction pour vérifier si un nombre est premier
+// Fonction pour vÃ©rifier si un nombre est premier
 int est_premier(int b) {
 	long i;
     if (b <= 1) {
@@ -17,18 +17,18 @@ int est_premier(int b) {
 
 int main() {
     int n;
-    printf("Entrez le numéro du nombre premier que vous voulez trouver (n) : ");
+    printf("Entrez le numÃ©ro du nombre premier que vous voulez trouver (n) : ");
     scanf("%d", &n);
 
-    int position = 0;  // Compteur pour les nombres premiers trouvés
-    int np_actu = 2;  // Le nombre à tester pour être premier
+    int position = 0;  // Compteur pour les nombres premiers trouvÃ©s
+    int np_actu = 2;  // Le nombre Ã  tester pour Ãªtre premier
 
     while (1) {
         if (est_premier(np_actu)) {
             position++;  // Si le nombre est premier, on l'ajoute au compteur
             if (position == n) {
-                printf("Le %d-ième nombre premier est : %d\n", n, np_actu);
-                break;  // On a trouvé le n-ième nombre premier, on sort de la boucle
+                printf("Le %d-iÃ¨me nombre premier est : %d\n", n, np_actu);
+                break;  // On a trouvÃ© le n-iÃ¨me nombre premier, on sort de la boucle
             }
         }
         np_actu++;  // On teste le nombre suivant
