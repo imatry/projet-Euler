c0dab0e7459e6931d86f817d37ae9d8abef1a749
imatry <ousssamafoot@gmail.com>
2025-03-11 11:24:41 +0000

    Update and rename Euler 06.c to Euler 06:Sum Square Difference.c

--- Euler 06:Sum Square Difference.c
+++ Euler 06:Sum Square Difference.c
@@ -5,29 +5,29 @@
 int N=100, i, Sc, S, Cs, Diff;
 int main(int argc, char *argv[]) {
 	
-//    la somme des carré
+//    la somme des carrÃ©
 	 for (i = 1; i <= 100; i++) {
     	/*printf(" %d ",i*i);*/
     	/*printf("\n");*/
         Sc += i*i;
     } 
-    printf("\nLa somme des carré des 100 premiers nombres naturels est %d\n",Sc);
+    printf("\nLa somme des carrÃ© des 100 premiers nombres naturels est %d\n",Sc);
     
     printf("\n");
-//    Le carré de la somme
+//    Le carrÃ© de la somme
     for (i = 1; i <= 100; i++) {
     	/*printf(" %d ",i*i);*/
 		S += i;
     	/*printf("\n");*/
         Cs = S*S ;
     }
-   printf("\nLe carré de la somme des 100 premiers nombres naturels est %d\n",Cs);
+   printf("\nLe carrÃ© de la somme des 100 premiers nombres naturels est %d\n",Cs);
 
  printf("\n");
-//    La différence entre Sc et Cs
+//    La diffÃ©rence entre Sc et Cs
     
     Diff= Cs-Sc;
-    printf("\nLa différence entre Sc et Cs des 100 premiers nombres naturels est %d\n",Diff);
+    printf("\nLa diffÃ©rence entre Sc et Cs des 100 premiers nombres naturels est %d\n",Diff);
 
 
 	return 0;
