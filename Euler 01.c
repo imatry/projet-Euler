66a6437b61f2921e1fe518fccbcabde51f15a3aa
imatry <ousssamafoot@gmail.com>
2025-03-11 10:43:55 +0000

    Add files via upload

---
+++ Euler 01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int i,S=0;
+int main(int argc, char *argv[]) {
+	for(i=0;i<1000;i++){ // 
+		if( i%3==0||i%5==0){//
+                       printf("%d\n",i);
+				S+=i;
+		}
+	}
+		printf(" la somme des multiples de 3 ou 5 sous 1000 est %d\n",S);
+	return 0;
+}
